b777d7dd2dfb30c4bf2789485b0bc50db2923347
lyamani-abdessamade <lyamaniabdessamade@gmail.com>
2026-06-30 02:25:01 +0100

    Update database connection password in Utilitaire.java

--- src/dao/Utilitaire.java
+++ src/dao/Utilitaire.java
@@ -13,7 +13,7 @@ public class Utilitaire {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             String url = "jdbc:mysql://localhost:3306/gestion_pharmacie";
-            conn = DriverManager.getConnection(url, "root", "h1a2j3a4r5");
+            conn = DriverManager.getConnection(url, "root", "mot-de-passe");
 
         } catch (Exception e) {
             System.out.println("Erreur connexion : " + e);
